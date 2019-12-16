94fa6e479e2fde4b319ebb3a17664668cd8dd720
ursulagg <isabellalealx@outlook.com>
2019-12-15 22:56:17 -0300

    organizando post e get

--- src/routes/medicamentosRoutes.js
+++ src/routes/medicamentosRoutes.js
@@ -2,7 +2,13 @@ const express = require("express")
 const router = express.Router()
 const controller = require("../controller/medicamentosController")
 
-
+/**
+ * @api {post}/ Adiciona
+ * @apiName Node Pharmacy
+ * @apiGroup Medicamentos
+ * 
+ * @apiSucess {String} Um medicamento será adicionado a farmácia.
+ */
 
 router.post("/", controller.post)   
 router.put("/", controller.put)
